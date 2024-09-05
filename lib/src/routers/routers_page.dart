104ef6b4253ea541a0a0d16834e4249f6ed39723
xplorel <http200404@gmail.com>
2024-09-05 23:54:11 +0800

    删除无用文件

--- lib/src/routers/routers_page.dart
+++ lib/src/routers/routers_page.dart
@@ -3,8 +3,6 @@ import 'package:gptassistant/src/home/custom_drag_list_page.dart';
 import 'package:gptassistant/src/editor/fleather_editor_screen.dart';
 import 'package:gptassistant/src/editor/quill_editor_screen.dart';
 import 'package:gptassistant/src/editor/quill_html_editor_screen.dart';
-import 'package:gptassistant/src/editor/super_editor_screen.dart';
-import 'package:gptassistant/src/editor/super_list_page.dart';
 import 'package:gptassistant/src/home/home_page.dart';
 import 'package:gptassistant/src/routers/routers_name.dart';
 
@@ -18,15 +16,8 @@ class RouterPage {
         name: RoutersName.quillHtmlEditorPage,
         page: () => const QuillHtmlEditorScreen()),
     GetPage(
-        name: RoutersName.superEditorPage,
-        page: () => const SuperEditorScreen()),
-    GetPage(
         name: RoutersName.fleatherEditorPage,
         page: () => const FleatherEditorScreen()),
-    GetPage(
-        name: RoutersName.enhancedEditorPage,
-        page: () => const FleatherEditorScreen()),
-    GetPage(name: RoutersName.superListPage, page: () => const SuperListPage()),
     GetPage(
         name: RoutersName.dragListPage, page: () => const CustomDragListPage()),
   ];
